4d07fed0d97bc7170e89ef32da11b48d1555bfdf
Xianwei <ios@yinxianwei.cn>
2015-07-04 21:31:04 +0800

    进度条

---
+++ JEWebViewControllerDemo/JEWebViewController.swift
@@ -0,0 +1,97 @@
+//
+//  JEWebViewController.swift
+//  JEWebViewControllerDemo
+//
+//  Created by Work on 15/7/4.
+//  Copyright (c) 2015年 尹现伟. All rights reserved.
+//  参考：https://github.com/coffellas-cto/GDWebViewController
+
+//MIT License
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import UIKit
+import WebKit
+
+class JEWebViewController: UIViewController {
+
+    var webView: WKWebView!
+    var urlString:String?
+    private var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        initWebView()
+    }
+    
+    func initWebView(){
+        self.webView = WKWebView()
+        webView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(self.webView)
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-0-[webView]-0-|", options: nil, metrics: nil, views: ["webView": webView]))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[webView]-0-|", options: nil, metrics: nil, views: ["webView": webView]))
+        
+        if self.urlString != nil{
+        let url = NSURL(string: self.urlString!)
+            let request = NSURLRequest(URL: url!)
+            webView.loadRequest(request)
+        }
+    }
+    
+    // MARK: KVO
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        switch keyPath {
+        case "estimatedProgress":
+            if let newValue = change[NSKeyValueChangeNewKey] as? NSNumber {
+                progressChanged(newValue)
+            }
+        default:
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        }
+    }
+    private func progressChanged(newValue: NSNumber) {
+        if progressView == nil {
+            progressView = UIProgressView()
+            progressView.setTranslatesAutoresizingMaskIntoConstraints(false)
+            self.view.addSubview(progressView)
+            
+            self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-0-[progressView]-0-|", options: nil, metrics: nil, views: ["progressView": progressView]))
+            self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[topGuide]-0-[progressView(2)]", options: nil, metrics: nil, views: ["progressView": progressView, "topGuide": self.topLayoutGuide]))
+        }
+        
+        progressView.progress = newValue.floatValue
+        if progressView.progress == 1 {
+            progressView.progress = 0
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                self.progressView.alpha = 0
+            })
+        } else if progressView.alpha == 0 {
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                self.progressView.alpha = 1
+            })
+        }
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        webView.stopLoading()
+    }
+}
--- JEWebViewControllerDemo/ViewController.swift
+++ JEWebViewControllerDemo/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func openUrl(sender: UIButton) {
+        var vc = JEWebViewController()
+        vc.urlString = "http://api.map.baidu.com/direction?origin=latlng:31.226177,121.546352%7Cname:%E6%88%91%E7%9A%84%E4%BD%8D%E7%BD%AE&destination=latlng:31.239516,121.472188%7Cname:%E7%BB%88%E7%82%B9&mode=driving&region=%E4%B8%8A%E6%B5%B7&output=html&src=%E4%B8%8A%E6%B5%B7%E7%BE%8E%E9%97%AE%E4%BF%A1%E6%81%AF%E6%8A%80%E6%9C%AF%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8%7C%E7%BE%8E%E9%97%AE"
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
